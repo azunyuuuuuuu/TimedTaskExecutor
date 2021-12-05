9af3d23232c6eb8b24104a51a27d3515fbd21723
azunyuuuuuuu <azunyuuuuuuu@users.noreply.github.com>
2021-12-05 19:37:42 +0100

    ➕ implemented logging to file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
 using TimedTaskExecutor;
 using TimedTaskExecutor.Services;
 
+string AppDataBasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), nameof(TimedTaskExecutor));
+
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddLogging(builder =>
+        {
+            builder.AddFile(Path.Combine(AppDataBasePath, "logs", "app_{0:yyyy}-{0:MM}-{0:dd}.log"), fileLoggerOpts =>
+            {
+                fileLoggerOpts.FormatLogFileName = fName => { return String.Format(fName, DateTime.UtcNow); };
+            });
+        });
         services.AddHostedService<ExecutionService>();
     })
     .Build();
