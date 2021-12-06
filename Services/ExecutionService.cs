e5718dbd78b0be95fad7eb8c1b6f591204fe893d
azunyuuuuuuu <azunyuuuuuuu@users.noreply.github.com>
2021-12-06 18:51:23 +0100

    🔧 fix bug task not being executed and extended logging

--- Services/ExecutionService.cs
+++ Services/ExecutionService.cs
@@ -10,7 +10,7 @@ public class ExecutionService : BackgroundService
     private readonly IHostApplicationLifetime _lifetime;
     private readonly ILogger<ExecutionService> _logger;
     private readonly IConfiguration _config;
-    private List<TaskDefinition>? _tasks = new List<TaskDefinition>();
+    private List<TaskDefinition> _tasks = new List<TaskDefinition>();
 
     public ExecutionService(IHostApplicationLifetime lifetime, ILogger<ExecutionService> logger, IConfiguration config)
     {
@@ -23,50 +23,59 @@ public class ExecutionService : BackgroundService
 
     private void InitializeConfiguration()
     {
-        var _tasks = _config.GetSection("Tasks").Get<List<TaskDefinition>>();
-        if (_tasks == null || _tasks.Count == 0)
+        var temp = _config.GetSection("Tasks").Get<List<TaskDefinition>>();
+
+        if (temp == null || temp.Count == 0)
         {
             _logger.LogError("No tasks defined");
             _lifetime.StopApplication();
         }
         else
         {
-            _logger.LogInformation($"{_tasks.Count} tasks found");
+            _tasks.AddRange(temp);
+            _logger.LogInformation($"{temp.Count} tasks found");
         }
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         await Task.Delay(3000, stoppingToken);
+        _logger.LogInformation("Start processing");
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            foreach (var task in _tasks ?? new List<TaskDefinition>())
+            _logger.LogTrace($"Processing loop start at {DateTimeOffset.Now}");
+
+            foreach (var task in _tasks)
             {
-                if (task.NextRuntime < DateTimeOffset.Now)
+                _logger.LogTrace($"Checking task {task}");
+                if (task.NextRuntime >= DateTimeOffset.Now)
+                    continue;
+
+                _logger.LogTrace($"Processing task {task}");
+
+                var expression = CronExpression.Parse(task.Schedule);
+                task.NextRuntime = expression.GetNextOccurrence(DateTimeOffset.Now, TimeZoneInfo.Local) ?? DateTimeOffset.Now;
+
+                try
                 {
-                    _logger.LogTrace($"Executing Task {task}");
+                    var results = await Cli.Wrap(task.ExecutablePath)
+                        .WithArguments(task.Arguments)
+                        .WithWorkingDirectory(task.WorkingDirectory)
+                        .ExecuteBufferedAsync(stoppingToken);
 
-                    var expression = CronExpression.Parse(task.Schedule);
-                    task.NextRuntime = expression.GetNextOccurrence(DateTimeOffset.Now, TimeZoneInfo.Local) ?? DateTimeOffset.Now;
-
-                    try
-                    {
-                        var results = await Cli.Wrap(task.ExecutablePath)
-                            .WithArguments(task.Arguments)
-                            .WithWorkingDirectory(task.WorkingDirectory)
-                            .ExecuteBufferedAsync(stoppingToken);
-
-                        _logger.LogTrace($"Task completed successfully {task}");
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex, $"Error during execution of {task}");
-                    }
+                    _logger.LogTrace($"Task completed successfully {task}");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Error during execution of {task}");
                 }
             }
 
+            _logger.LogTrace($"Processing loop end at {DateTimeOffset.Now}");
             await Task.Delay(1000, stoppingToken);
         }
+
+        _logger.LogInformation("End processing");
     }
 }
